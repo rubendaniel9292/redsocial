658c44c3000d6ea880e0895065bda3f7ea466690
Ruebn Daniel <rubenrivas_17@hotmail.com>
2024-04-21 22:23:26 -0500

    Actualizar configuración base para despliegue 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base:'https://rubendaniel9292.github.io/redsocial/'
-  base:'/'
+  base:'https://rubendaniel9292.github.io/redsocial/'
+  //base:'/'
 })
