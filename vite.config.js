bb0d2152f638c7b7afb9585466d9c724545fe3c9
Ruebn Daniel <rubenrivas_17@hotmail.com>
2024-04-21 21:36:29 -0500

    cambio menores para despliege

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://rubendaniel9292.github.io/redSocial/'
+  base:'https://rubendaniel9292.github.io/redsocial/'
 })
